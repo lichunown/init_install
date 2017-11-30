b78f466948a3859f0df734dcfa43acf8676d065c
LCY <lichunyang_1@outlook.com>
2017-11-30 15:16:57 +0800

    ubuntu16

--- init_start.sh
+++ init_start.sh
@@ -1,15 +1,15 @@
 xhost +
 sudo mv /etc/apt/sources.list /etc/apt/sources.list.back
-sudo sh -c 'echo "deb http://linux.xidian.edu.cn/mirrors/ubuntu/ artful main restricted universe multiverse\n\
-deb-src http://linux.xidian.edu.cn/mirrors/ubuntu/ artful main restricted universe multiverse\n\
-deb http://linux.xidian.edu.cn/mirrors/ubuntu/ artful-security main restricted universe multiverse\n\
-deb-src http://linux.xidian.edu.cn/mirrors/ubuntu/ artful-security main restricted universe multiverse\n\
-deb http://linux.xidian.edu.cn/mirrors/ubuntu/ artful-updates main restricted universe multiverse\n\
-deb-src http://linux.xidian.edu.cn/mirrors/ubuntu/ artful-updates main restricted universe multiverse\n\
-deb http://linux.xidian.edu.cn/mirrors/ubuntu/ artful-backports main restricted universe multiverse\n\
-deb-src http://linux.xidian.edu.cn/mirrors/ubuntu/ artful-backports main restricted universe multiverse\n\
-deb http://linux.xidian.edu.cn/mirrors/ubuntu/ artful-proposed main restricted universe multiverse\n\
-deb-src http://linux.xidian.edu.cn/mirrors/ubuntu/ artful-proposed main restricted universe multiverse" >>  /etc/apt/sources.list'
+sudo sh -c 'echo "deb http://linux.xidian.edu.cn/mirrors/ubuntu/ xenial main restricted universe multiverse\n\
+deb-src http://linux.xidian.edu.cn/mirrors/ubuntu/ xenial main restricted universe multiverse\n\
+deb http://linux.xidian.edu.cn/mirrors/ubuntu/ xenial-security main restricted universe multiverse\n\
+deb-src http://linux.xidian.edu.cn/mirrors/ubuntu/ xenial-security main restricted universe multiverse\n\
+deb http://linux.xidian.edu.cn/mirrors/ubuntu/ xenial-updates main restricted universe multiverse\n\
+deb-src http://linux.xidian.edu.cn/mirrors/ubuntu/ xenial-updates main restricted universe multiverse\n\
+deb http://linux.xidian.edu.cn/mirrors/ubuntu/ xenial-backports main restricted universe multiverse\n\
+deb-src http://linux.xidian.edu.cn/mirrors/ubuntu/ xenial-backports main restricted universe multiverse\n\
+deb http://linux.xidian.edu.cn/mirrors/ubuntu/ xenial-proposed main restricted universe multiverse\n\
+deb-src http://linux.xidian.edu.cn/mirrors/ubuntu/ xenial-proposed main restricted universe multiverse" >>  /etc/apt/sources.list'
 sudo apt update
 
 sudo sh ./install_program.sh
--- install_program.sh
+++ install_program.sh
@@ -1,18 +1,8 @@
-sudo apt install -y vim
-git config --global credential.helper store
 
-sudo apt install -y git
-
-# install chrome
-sudo wget http://www.linuxidc.com/files/repo/google-chrome.list -P /etc/apt/sources.list.d/
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub  | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install -y google-chrome-stable
 
 # install sublime2
-if [! -f "Sublime\ Text\ 2.0.2\ x64.tar.bz2"]; then
-	wget http://c758482.r82.cf2.rackcdn.com/Sublime\ Text\ 2.0.2\ x64.tar.bz2
-fi
+
+wget http://c758482.r82.cf2.rackcdn.com/Sublime\ Text\ 2.0.2\ x64.tar.bz2
 tar vxjf Sublime\ Text\ 2.0.2\ x64.tar.bz2
 sudo mv Sublime\ Text\ 2 /opt/
 sudo ln -s /opt/Sublime\ Text\ 2/sublime_text /usr/bin/sublime
@@ -37,9 +27,9 @@ sudo apt-get install -y sqlite3
 sudo apt-get install -y libsqlite3-dev
 
 # glib
-if [! -f "./glib-2.54.2.tar.xz"]; then
-	wget http://ftp.gnome.org/pub/gnome/sources/glib/2.54/glib-2.54.2.tar.xz
-fi
+
+wget http://ftp.gnome.org/pub/gnome/sources/glib/2.54/glib-2.54.2.tar.xz
+
 tar -xvJf ./glib-2.54.2.tar.xz
 cd ./glib-2.54.2
 
