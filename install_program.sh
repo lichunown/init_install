bd913787b466e8cad1f52fad4852dca0f02c2b42
LCY <lichunyang_1@outlook.com>
2017-12-05 19:52:19 +0800

    default: not install sublime2

--- install_program.sh
+++ install_program.sh
@@ -6,26 +6,26 @@ git config --global user.email "lichunyang_1@outlook.com"
 git config --global user.name "LCY"
 git config --global credential.helper store
 
-# install sublime2
-wget http://c758482.r82.cf2.rackcdn.com/Sublime\ Text\ 2.0.2\ x64.tar.bz2
-tar vxjf Sublime\ Text\ 2.0.2\ x64.tar.bz2
-sudo mv Sublime\ Text\ 2 /opt/
-sudo ln -s /opt/Sublime\ Text\ 2/sublime_text /usr/bin/sublime
-echo "[Desktop Entry]\n\
-Version=1.0\n\
-Type=Application\n\
-Name=sublime\n\
-Name[zh_CN]=sublime\n\
-Comment=sublime\n\
-Exec=/opt/Sublime\ Text\ 2/sublime_text\n\
-Icon=/opt/Sublime\ Text\ 2/Icon/48x48/sublime_text.png\n\
-Terminal=false\n\
-StartupNotify=true\n\
-X-DBUS-ServiceName=\n\
-X-DBUS-StartupType=\n\
-X-KDE-SubstituteUID=false\n\
-X-KDE-Username=owen" >  ~/sublime.desktop
-chmod +x ~/sublime.desktop
+## install sublime2
+#wget http://c758482.r82.cf2.rackcdn.com/Sublime\ Text\ 2.0.2\ x64.tar.bz2
+#tar vxjf Sublime\ Text\ 2.0.2\ x64.tar.bz2
+#sudo mv Sublime\ Text\ 2 /opt/
+#sudo ln -s /opt/Sublime\ Text\ 2/sublime_text /usr/bin/sublime
+#echo "[Desktop Entry]\n\
+#Version=1.0\n\
+#Type=Application\n\
+#Name=sublime\n\
+#Name[zh_CN]=sublime\n\
+#Comment=sublime\n\
+#Exec=/opt/Sublime\ Text\ 2/sublime_text\n\
+#Icon=/opt/Sublime\ Text\ 2/Icon/48x48/sublime_text.png\n\
+#Terminal=false\n\
+#StartupNotify=true\n\
+#X-DBUS-ServiceName=\n\
+#X-DBUS-StartupType=\n\
+3X-KDE-SubstituteUID=false\n\
+#X-KDE-Username=owen" >  ~/sublime.desktop
+#chmod +x ~/sublime.desktop
 
 # sqlite3
 sudo apt-get install -y sqlite3 
